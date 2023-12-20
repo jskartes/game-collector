5b5be12d3d003ce5dab43d16c689696793e75930
jskartes <joshuakartes@gmail.com>
2023-12-20 16:34:09 -0700

    create Game data model and migrate to database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    players = models.CharField(max_length=20)
+    playing_time = models.CharField(max_length=100)
+    complexity = models.DecimalField(max_digits=3, decimal_places=2)
