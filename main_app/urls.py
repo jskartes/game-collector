7efaa13d3457c692f1be6ddade0d5cb4b65a7174
jskartes <joshuakartes@gmail.com>
2023-12-21 16:34:36 -0700

    refactor link hrefs in templates to use url names

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('games/', views.games_index, name='index'),
-    path('games/<int:game_id>/', views.games_detail, name='detail'),
-    path('games/create/', views.GameCreate.as_view(), name='create'),
+    path('games/', views.games_index, name='games_index'),
+    path('games/<int:game_id>/', views.games_detail, name='games_detail'),
+    path('games/create/', views.GameCreate.as_view(), name='games_create'),
 ]
